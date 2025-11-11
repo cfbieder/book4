9199fca07382d86d75577dff98361f232d11208a
Chris Biedermann <cfbieder@hotmail.com>
2025-11-11 20:05:57 +0100

    update

---
+++ server/server.js
@@ -0,0 +1,21 @@
+/******************************************************************************************************
+ * Data Server
+ * Chris Biedermann
+ * V1.0
+ * November 2025
+ * 
+ * Links to app.js 
+ * Link the actual server to routing code in app.js
+ *******************************************************************************************************/
+
+//App.js contains main functions
+//const app = require("./app");
+
+
+
+//Start REST Server
+console.log("Listening")
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`[DA] Server Started: Running on port ${port}`);
+});
